3d51a12abf6178755129d2a3955b6b90d3ec4cd4
Elin Alm <elin95alm@gmail.com>
2020-04-14 16:11:56 +0200

    mycket

---
+++ public/logic.js
@@ -0,0 +1,45 @@
+// fetch("http://localhost:3000/dogs").then((response) => {
+//     return response.json()
+// }).then((dogs) => {
+//     console.log(dogs)
+//     showAllDogs(dogs)
+//     findDog(dog)
+// })
+
+// function showAllDogs(dogs){
+//     let allDogs = document.getElementById("allDogs")
+
+//     dogs.forEach(dog => {
+       
+//         let dogBreed = document.createElement("h4")
+//         let dogAge = document.createElement("h4")
+//         dogBreed.innerText = dog.breed
+//         dogAge.innerText = dog.age
+
+//         let dogDiv = document.createElement("div")
+//         dogDiv.appendChild(dogBreed)
+//         dogDiv.appendChild(dogAge)
+
+//         allDogs.appendChild(dogDiv)
+
+
+//     });
+// }
+
+// function findDog(dog){
+//     const id = document.getElementById("dogId").value
+//     console.log(id);
+
+//     let specificDog = document.getElementById("specificDog")
+//     specificDog.innerHTML = ""
+    
+//     if(dog) {
+        
+//     } else {
+//         let errorResponse = document.createElement("h4")
+//         errorResponse.innerText = "Det finns ingen hund med detta nummer :/"
+//         specificDog.appendChild(errorResponse)
+//     }
+    
+
+// }
---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require ('express')
+const app = express()
+
+const dogs = [
+        {
+        id: 1,
+        breed: 'Labrador',
+        age: 10,
+    }, {
+        id: 2,
+        breed: 'Retriver',
+        age: 5,
+    },
+    {
+        id: 3,
+        breed: 'Flatcoat retriver',
+        age: 4,
+    }
+]
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/dogs/', (req, res) => {
+    res.json(dogs)
+})
+
+
+// app.get('/dogs/:id', (req, res) => {
+//     const foundDog = dogs.find((dog) => {
+//         if(dog.id.toString() === req.params.id) {
+//             return true
+//         } else {
+//             return false
+//         }
+//     }
+// )
+//     if(!foundDog) {
+//         res.status(404).send()
+//     } else {
+//         res.send(foundDog)
+//     }
+
+// })
+
+app.post('/dogs/', (req, res) => {
+    dogs.push(req.body)
+    res.status(201)
+    res.send()
+})
+
+app.put('/dogs/:id', (req, res) => {
+    console.log(req.body);
+    
+    const foundDog = dogs.find(d => d.id == req.params.id) 
+        if(!foundDog) {
+            return res.status(404).send()
+        } 
+        foundDog.breed = req.body.breed
+        foundDog.age = req.body.age
+
+    // foundDog = {
+    //     id: req.params.id,
+    //     breed: req.body.breed,
+    //     age: req.body.age
+    // }
+        res.send(foundDog)
+})
+
+//delete 
+
+
+app.listen(3000, 'localhost', () => {
+    console.log('Server is up and running');
+    
+})
